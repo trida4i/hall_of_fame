b815c0aeba651f324b5dc6015ddd9e6f6e2c785f
Redwoodstudio <redwoodstudio@seznam.cz>
2024-04-26 16:31:21 +0200

    initial

---
+++ main.js
@@ -0,0 +1,13 @@
+
+var grid = document.querySelector('.grid');
+var msnry;
+
+imagesLoaded( grid, function() {
+  // init Isotope after all images have loaded
+  msnry = new Masonry( grid, {
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true,
+    gutter: '.gutter-sizer',
+  });
+});
